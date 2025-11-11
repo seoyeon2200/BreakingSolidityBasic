3a9a4827b41b8de9606006663895b8d66489de48
Lee Seoyeon <yellow917330@gmail.com>
2025-11-11 23:21:57 +0900

    solidity 학습

--- lec2.sol
+++ lec2.sol
@@ -1,43 +1,40 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.7.0 <0.9.0;
 
-pragma solidity >=0.7.0 < 0.9.0;
-/*
-  data type
-  boolean, bytes, address, uint
-  
-  reference type
-  string, Arrays, struct
-  
-  mapping type
-*/
 contract lec2{
-    // bolean : true/false
+    //data type
+    //boolean, bytes, address, uint
+
+    //reference type
+    //string, Arrays, struct
+
+    //mapping type
+
+    //boolean: true / false
     bool public b = false;
-    
-    // ! || == != &&
-    bool public b1 = !false; //true
-    bool public b2 = false || true; // true
-    bool public b3 = false == true; //false
-    bool public b4 = false != false; // false
-    bool public b5 = false && true; // false
-    
+
+    bool public b1 = !false;
+    bool public b2 = false||true;
+    bool public b3 = false == true;
+    bool public b4 = false && true;
+
     //bytes 1~32
     bytes4 public bt = 0x12345678;
-    bytes public bt2 = "STRING";
-    
-    //address 
-    address public addr = 0xd9145CCE52D386f254917e481eB44e9943F39138;
-    
-    // int(음수 포함) Vs uint (음수 안 포함)
-    
-    //int8 : -2^7 ~ (2^7)-1
-    int8 public it = -4;
-    //uint8 : 0 ~ 2^8-1
-    uint8 public it2 = 123;
-    
-    //uint256 : 0 ~ 2^256-1
-    uint256 public it3 = 123;
+    bytes public bt2 = "STRING"; //byte만 넣어서 string 저장
+
+    //address : ether에서 송수신할때 이용하는 계좌, 스마트 컨트랙트를 배포할 때도 발생
+    address public addr = 0x358AA13c52544ECCEF6B0ADD0f801012ADAD5eE3;
+
+    //int vs uint (-마이너스가 있냐 없냐에 따라 달라짐)
+    //int
+    //-2^7-1 ~2^7-1
+    int8 public it = 4;
+
+    //uint
+    //0~2^8-1
+    uint256 public uit = 132213;
+
+    //+-*/
+
+    uint8 public uit2 = 256;
 }
-
-
-
